CyanogenMod/android_device_sony_common

init: Fix recovery without FOTA and cleanup sources
 * Restructure extract_ramdisk.cpp to delete exit() breakpoints
 * KEYCHECK_RECOVERY: CM_ONLY or FOTA_CM to be more explicit
 * Prevent unpacking recovery if the cpio file is missing

Change-Id: I07d4995ad05b6bdfde33f98368da356c9e2565ac
Signed-off-by: AdrianDC <radian.dc@gmail.com>

init/init_main.cpp
@@ -81,13 +81,13 @@ int main(int argc, char** __attribute__((unused)) argv)
 
         // Retrieve keycheck result
         keycheckStatus = system_exec_kill(keycheck_pid, KEYCHECK_TIMEOUT);
-        recoveryBoot = (keycheckStatus == KEYCHECK_RECOVERY_BOOT ||
-                keycheckStatus == KEYCHECK_RECOVERY_FOTA);
+        recoveryBoot = (keycheckStatus == KEYCHECK_RECOVERY_CM_ONLY ||
+                keycheckStatus == KEYCHECK_RECOVERY_FOTA_CM);
     }
     else
     {
-        // Direct boot to FOTA / Boot Recovery
-        keycheckStatus = KEYCHECK_RECOVERY_FOTA;
+        // Direct boot to FOTA / CM Recovery
+        keycheckStatus = KEYCHECK_RECOVERY_FOTA_CM;
     }
 
     // Boot to Recovery
@@ -99,7 +99,7 @@ int main(int argc, char** __attribute__((unused)) argv)
 
         // FOTA Recovery importation
         if (DEV_BLOCK_FOTA_NUM != -1 &&
-                keycheckStatus != KEYCHECK_RECOVERY_BOOT)
+                keycheckStatus != KEYCHECK_RECOVERY_CM_ONLY)
         {
             mknod(DEV_BLOCK_FOTA_PATH, S_IFBLK | 0600,
                     makedev(DEV_BLOCK_MAJOR, DEV_BLOCK_FOTA_NUM));
@@ -111,9 +111,12 @@ int main(int argc, char** __attribute__((unused)) argv)
         }
 
         // Recovery ramdisk
-        const char* argv_ramdiskcpio[] = { EXEC_TOYBOX, "cpio", "-i", "-F",
-                SBIN_CPIO_RECOVERY, nullptr };
-        system_exec(argv_ramdiskcpio);
+        if (file_exists(SBIN_CPIO_RECOVERY))
+        {
+            const char* argv_ramdiskcpio[] = { EXEC_TOYBOX, "cpio", "-i", "-F",
+                    SBIN_CPIO_RECOVERY, nullptr };
+            system_exec(argv_ramdiskcpio);
+        }
     }
     // Boot to Android
     else
@@ -123,9 +126,12 @@ int main(int argc, char** __attribute__((unused)) argv)
         init_board.introduce_android();
 
         // Unpack Android ramdisk
-        const char* argv_ramdiskcpio[] = { EXEC_TOYBOX, "cpio", "-i", "-F",
-                SBIN_CPIO_ANDROID, nullptr };
-        system_exec(argv_ramdiskcpio);
+        if (file_exists(SBIN_CPIO_ANDROID))
+        {
+          const char* argv_ramdiskcpio[] = { EXEC_TOYBOX, "cpio", "-i", "-F",
+                  SBIN_CPIO_ANDROID, nullptr };
+          system_exec(argv_ramdiskcpio);
+        }
     }
 
     // Finish init outputs

oshmoun/android_hardware_sony_init

 * KEYCHECK_RECOVERY: BOOT_ONLY or FOTA_BOOT to be more explicit

init/init_main.cpp
@@ -81,13 +81,13 @@ int main(int argc, char** __attribute__((unused)) argv)
 
         // Retrieve keycheck result
         keycheckStatus = system_exec_kill(keycheck_pid, KEYCHECK_TIMEOUT);
-        recoveryBoot = (keycheckStatus == KEYCHECK_RECOVERY_BOOT ||
-                keycheckStatus == KEYCHECK_RECOVERY_FOTA);
+        recoveryBoot = (keycheckStatus == KEYCHECK_RECOVERY_BOOT_ONLY ||
+                keycheckStatus == KEYCHECK_RECOVERY_FOTA_BOOT);
     }
     else
     {
         // Direct boot to FOTA / Boot Recovery
-        keycheckStatus = KEYCHECK_RECOVERY_FOTA;
+        keycheckStatus = KEYCHECK_RECOVERY_FOTA_BOOT;
     }
 
     // Boot to Recovery
@@ -99,7 +99,7 @@ int main(int argc, char** __attribute__((unused)) argv)
 
         // FOTA Recovery importation
         if (DEV_BLOCK_FOTA_NUM != -1 &&
-                keycheckStatus != KEYCHECK_RECOVERY_BOOT)
+                keycheckStatus != KEYCHECK_RECOVERY_BOOT_ONLY)
         {
             mknod(DEV_BLOCK_FOTA_PATH, S_IFBLK | 0600,
                     makedev(DEV_BLOCK_MAJOR, DEV_BLOCK_FOTA_NUM));
@@ -111,9 +111,12 @@ int main(int argc, char** __attribute__((unused)) argv)
         }
 
         // Recovery ramdisk
-        const char* argv_ramdiskcpio[] = { EXEC_TOYBOX, "cpio", "-i", "-F",
-                SBIN_CPIO_RECOVERY, nullptr };
-        system_exec(argv_ramdiskcpio);
+        if (file_exists(SBIN_CPIO_RECOVERY))
+        {
+            const char* argv_ramdiskcpio[] = { EXEC_TOYBOX, "cpio", "-i", "-F",
+                    SBIN_CPIO_RECOVERY, nullptr };
+            system_exec(argv_ramdiskcpio);
+        }
     }
     // Boot to Android
     else
@@ -123,9 +126,12 @@ int main(int argc, char** __attribute__((unused)) argv)
         init_board.introduce_android();
 
         // Unpack Android ramdisk
-        const char* argv_ramdiskcpio[] = { EXEC_TOYBOX, "cpio", "-i", "-F",
-                SBIN_CPIO_ANDROID, nullptr };
-        system_exec(argv_ramdiskcpio);
+        if (file_exists(SBIN_CPIO_ANDROID))
+        {
+            const char* argv_ramdiskcpio[] = { EXEC_TOYBOX, "cpio", "-i", "-F",
+                    SBIN_CPIO_ANDROID, nullptr };
+            system_exec(argv_ramdiskcpio);
+        }
     }
 
     // Finish init outputs