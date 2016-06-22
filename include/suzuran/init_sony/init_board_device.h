d0c761eeba0345316e8c648be198df5192fa740f
oshmoun <oshmoun100@googlemail.com>
2016-06-22 02:33:01 +0200

    suzuran: adjust headers to use keycheck and leds

--- include/suzuran/init_sony/init_board_device.h
+++ include/suzuran/init_sony/init_board_device.h
@@ -20,6 +20,9 @@
 #include "init_board_common.h"
 #include "init_prototypes.h"
 
+// Constants: device LEDs
+#include "sony_lights.h"
+
 // Constants: devices controls
 #define DEV_BLOCK_FOTA_NUM 32
 
@@ -28,17 +31,44 @@
 
 // Constants: init configurations
 #define FOTA_RAMDISK_CHECK 0
-#define KEYCHECK_ENABLED 0
+#define KEYCHECK_ENABLED 1
 
 // Class: init_board_device
 class init_board_device : public init_board_common
 {
 public:
-    // Board: Finish init
+    // Board: introduction for keycheck
+    virtual void introduce_keycheck()
+    {
+        // LED boot selection colors
+        led_color(255, 0, 255);
+    }
+
+    // Board: introduction for Android
+    virtual void introduce_android()
+    {
+        // Power off LED
+        led_color(0, 0, 0);
+    }
+
+    // Board: introduction for Recovery
+    virtual void introduce_recovery()
+    {
+        // LED Recovery colors
+        led_color(255, 100, 0);
+    }
+
+    // Board: set led colors
+    void led_color(uint8_t r, uint8_t g, uint8_t b)
+    {
+        write_int(LED_RED_PATH "brightness", r);
+        write_int(LED_GREEN_PATH "brightness", g);
+        write_int(LED_BLUE_PATH "brightness", b);
+    }
+     // Board: Finish init
     virtual void finish_init()
     {
         // Rename init
-        unlink("/init");
         rename("/init.real", "/init");
     }
 };
